50757cce8174b8698037df2b2d39878b51616bc7
WianB <wianbotha@gmail.com>
2017-03-09 12:01:49 +0200

    Cat V2

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
-var catCounter = 0;
-
+var cat1Counter = 0;
+var cat2Counter = 0;
 
 $(document).ready(function() {
-    $('.cat-img').click(function() {
-        console.log("test");
-        catCounter++;
-        $('.cat-count').text('Current count is: ' + catCounter);
+    //Append text
+    $('.cat1').text('Cat 1');
+    $('.cat2').text('Cat 2');
+
+    //Listeners for clicking
+    $('.cat-img1').click(function() {
+        cat1Counter++;
+        $('.cat1-count').text('Current count for cat 1 is: ' + cat1Counter);
+    });
+
+    $('.cat-img2').click(function() {
+        cat2Counter++;
+        $('.cat2-count').text('Current count for cat 2 is: ' + cat2Counter);
     });
 });
