b1fdbdc58873b2b773e6d964681473b488ba462c
WianB <wianbotha@gmail.com>
2017-03-15 16:17:58 +0200

    New commits of misc updates

--- js/script.js
+++ js/script.js
@@ -1,19 +1,50 @@
 var cat1Counter = 0;
 var cat2Counter = 0;
 
+var catName = ["cat1", "cat2", "cat3", "cat4", "cat5"];
+
+var Cat = function(input){
+  this.print = input;
+  this.count =0;
+};
+
+/*
+Cat.prototype.event = function(src){
+  this.addEventListener('click', function() {
+      $('cat-img').attr('src', src);
+  });
+};
+
+*/
+
+Cat.prototype.IncreaseCount = function(){
+  this.count++;
+};
+
+Cat.prototype.returnCount = function(){
+  return this.count;
+};
+
+Cat.prototype.returnPrint = function(){
+  return this.print;
+};
+
 $(document).ready(function() {
-    //Append text
-    $('.cat1').text('Cat 1');
-    $('.cat2').text('Cat 2');
+    //main loop for all the cats
+    for (var i = 0; i < catName.length; i++) {
+        var elem = '<li class ="cat' + i + '-list">' + catName[i] + '</li>';
+        $('.cat-list').append(elem);
+      };
 
-    //Listeners for clicking
-    $('.cat-img1').click(function() {
-        cat1Counter++;
-        $('.cat1-count').text('Current count for cat 1 is: ' + cat1Counter);
-    });
+    //add event Listeners
+    var Cats = document.getElementsByTagName("li");
+    console.log(Cats);
+    for(var i = 0; i < Cats.length; i++){
+      Cats[i].addEventListener('click', function() {
+          $('cat-img').attr('src', 'img/cat' + i +'.jpg');
+      });
+    };
 
-    $('.cat-img2').click(function() {
-        cat2Counter++;
-        $('.cat2-count').text('Current count for cat 2 is: ' + cat2Counter);
-    });
+      console.log(Cats);
+
 });
